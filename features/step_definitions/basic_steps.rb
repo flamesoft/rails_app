ceaad451419f0de56ccfd0ae8ed1356c880605c3
Carl-Philip <callea2@gmail.com>
2016-05-25 11:55:18 +0200

    Finished label refactoring

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -66,17 +66,17 @@ end
 
 Given(/^I fill in dummy user email and password$/) do
   steps %Q{
-    And I fill in "user_email" with "test@gmail.com"
-    And I fill in "user_password" with "12345678"
-    And I fill in "user_password_confirmation" with "12345678"
+    And I fill in "Email" with "test@gmail.com"
+    And I fill in "Password" with "12345678"
+    And I fill in "Password confirmation" with "12345678"
   }
 end
 
 Given(/^I am logged\-in as Jenny$/) do
   steps %Q{
     Given I am on the "login page"
-    And I fill in "user_email" with "jenny@gmail.com"
-    And I fill in "user_password" with "password"
+    And I fill in "Email" with "jenny@gmail.com"
+    And I fill in "Password" with "password"
     And I click on the "Log in" button
   }
 end
@@ -84,8 +84,8 @@ end
 Given(/^Daniel sent a mail and signed out$/) do
   steps %Q{
     Given I am on the "login page"
-    And I fill in "user_email" with "daniel@gmail.com"
-    And I fill in "user_password" with "password"
+    And I fill in "Email" with "daniel@gmail.com"
+    And I fill in "Password" with "password"
     And I click on the "Log in" button
     And I click on the "Inbox" link
     And I click on the "Compose" link
