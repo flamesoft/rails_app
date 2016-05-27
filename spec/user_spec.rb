1a9775b7caf01a2581da5523ab4b072903fedec6
Yan Zhang <emma2740@gmail.com>
2016-05-27 10:28:11 +0200

    Add unit tests for user.rb

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,8 +1,15 @@
 require 'rails_helper.rb'
 
 RSpec.describe User , type: :model do
+  subject { described_class.new(name: 'Yan', email:'y@gmail.com')}
   describe 'Validations of name' do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_length_of(:name).is_at_most(10) }
+  end
+  it 'is expected to have a mailboxer_name' do
+    expect(:mailboxer_name).not_to be nil
+  end
+  it 'is expected to have a mailboxer_email' do
+    expect(:mailboxer_email).not_to be nil
   end
 end
